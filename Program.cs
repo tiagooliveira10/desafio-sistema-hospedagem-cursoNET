a1a5314cb1eca42b34a50ae56883ada6b68fbec9
Tiago Oliveira <tiagosilva.oliveira@outlook.com.br>
2022-11-12 12:37:16 -0300

    Desafio hospedagem

--- Program.cs
+++ Program.cs
@@ -13,13 +13,14 @@ hospedes.Add(p1);
 hospedes.Add(p2);
 
 // Cria a suíte
-Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+Suite suite = new Suite(tipoSuite: "Premium", capacidade: 10, valorDiaria: 30);
 
 // Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
+Reserva reserva = new Reserva(diasReservados: 15);
 reserva.CadastrarSuite(suite);
 reserva.CadastrarHospedes(hospedes);
 
 // Exibe a quantidade de hóspedes e o valor da diária
+Console.WriteLine($"Suíte tipo: {suite.TipoSuite}");
 Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
-Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+Console.WriteLine($"Valor da diária: {reserva.CalcularValorDiaria()}");
